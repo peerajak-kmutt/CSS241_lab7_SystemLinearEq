f784117f3a28a9e9971109c3347b5ad80bcf2204
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-11-03 16:15:56 +0700

    Update Prob1_test.py

--- Prob1_test.py
+++ Prob1_test.py
@@ -2,10 +2,13 @@ import Prob1 as p1
 import numpy as np
 
 def test_1():
-    one3 = p1.Problem1(3)
-    assert (one3 == np.array([[1.,0.,0.],[1.,1.,0.],[1.,1.,1.]])).all()
+    n = 4
+    Aa = [[6,-2,2,4],[12,-8,6,10],[3,-13,9,3],[-6,4,1,-18]]
+    bb = [16,26,-19,-34]
+    A = np.array(Aa,dtype=np.float64)
+    b = np.array(bb, dtype=np.float64)
+    x = p1.Problem1(A,b)
+    assert (x == np.array([ 3.,  1., -2.,  1.]).all()
 
-def test_2():
-    one4 = p1.Problem1(4)
-    assert (one4 == np.array([[1.,0.,0.,0.],[1.,1.,0.,0.],[1.,1.,1.,0.],[1.,1.,1.,1.]])).all()
+
 
