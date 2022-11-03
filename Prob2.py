93cdf50e38e2e9ab1ef9b9f533ce8b00549eb499
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-11-03 16:23:45 +0700

    Update Prob2.py

--- Prob2.py
+++ Prob2.py
@@ -1,16 +1,31 @@
-def Reduce_this_to_NestedListComprehension(N):
-    primes = []
-    for num in range(1,N):
-        list_of_divisables = []
-        for d in range(2,num):
-            if num%d == 0: #divisable by some number
-                list_of_divisables.append(d)
-        if not list_of_divisables:
-            primes.append(num)
-    return primes        
+import numpy as np
 
+def GaussianSwapForwardElimination(A,b):
+    m,n = A.shape
+    bn, = b.shape
+    assert n==m==bn
+    Aans = A.copy()
+    bans = b.copy()
+    l = list(range(n))
+    sl = np.abs(A).max(axis=1)
 
-def Problem2(N):
-    primes = #Your code here One Line only
-    return primes
+    #Your Code Here
+    # End your code
+
+    
+    return Aans,bans,l
+
+def GaussianSwapBacksubstitution(A,b,l):
+    m,n = A.shape
+    bn, = b.shape
+    assert n==m==bn
+    x = np.zeros(n,dtype=np.float64)
+    #Your Code Here
+    # End your code
+    return x
+
+def Problem2(A,b):
+    Aselem,bselem,l = GaussianSwapForwardElimination(A,b)
+    x = GaussianSwapBacksubstitution(Aselem,bselem,l)
+    return x
 
