a1cea532f3cbd15cab09fd5a8abf938420915882
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-11-03 16:12:17 +0700

    Update Prob1.py

--- Prob1.py
+++ Prob1.py
@@ -1,9 +1,29 @@
 import numpy as np
 
+def GaussianForwardElimination(A,b): #A,b are numpy array of size (n,n) and n for equation Ax=b
+    m,n = A.shape
+    bn, = b.shape
+    assert n==m==bn
+    Aans = A.copy()
+    bans = b.copy()
 
-def Problem1(n): 
-    onesk = np.ones((n,n))
-    indx = #Your code here
-    xyindx = tuple(zip(*indx))
-    onesk[xyindx] = 0
-    return onesk
+    #Your Code Here
+    #End Your code
+    
+    return Aans,bans
+
+def GaussianBacksubstitution(A,b):
+    m,n = A.shape
+    bn, = b.shape
+    assert n==m==bn
+    x = np.zeros(n,dtype=np.float64)
+    
+    #Your Code Here
+    #End Your code
+    
+    return x
+
+def Problem1(A,b): 
+    Aelem, belem = GaussianForwardElimination(A,b)
+    x = GaussianBacksubstitution(Aelem,belem)
+    return x
